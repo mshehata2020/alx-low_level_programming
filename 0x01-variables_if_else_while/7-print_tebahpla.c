55f7bc55ca51e5a449126369aab9c07e44029213
mshehata920 <mahmoudshehata391@gmail.com>
2023-02-19 12:51:37 -0800

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/** 
+ *reverse alphabet
+ *place every chare
+ *print it out
+ */
+int main(void)
+{
+  char ch;
+  for(ch=z;ch=a;ch--)
+    {
+      putchar(ch);
+	}
+  putchar('\n');
+	return (0);
+
+}
