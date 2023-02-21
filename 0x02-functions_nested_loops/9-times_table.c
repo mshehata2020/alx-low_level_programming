e3f7464d53b1b091f3ddb5948cd8ce92d29e26a5
mshehata920 <mahmoudshehata391@gmail.com>
2023-02-21 10:43:46 -0800

    new

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *make while and for
+ *multyply each of them
+ /*
+void times_table(void);
+int j=0,i=0,d;
+whil(i<=9)
+{
+for(j=0,j=9,j++)
+{
+d=i*j;
+_putchar("%d",&d);
+}
+i++;
+}
