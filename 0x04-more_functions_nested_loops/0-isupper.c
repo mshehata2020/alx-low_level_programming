b9af9c9deb7c703045208fb683ff7f3e4beab641
mshehata920 <mahmoudshehata391@gmail.com>
2023-02-23 12:12:39 -0800

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks is a character is uppercase
+ * integer
+ * Return: 1 if true
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
