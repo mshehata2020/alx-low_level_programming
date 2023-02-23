f25f2694f0e1529a7b3bfd074500dc83b2aebf8e
mshehata920 <mahmoudshehata391@gmail.com>
2023-02-23 12:15:27 -0800

    new

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * _isupper - checks is a character is uppercase
- * integer
- * Return: 1 if true
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
  */
 
 int _isupper(int c)
